de639b3833bb9fefeffe41d89d90bcdacdaf12b6
yuulye <yuulye@gmail.com>
2025-02-22 20:39:39 +0700

    update logo; burger menu implementation; theme color updated

---
+++ main.js
@@ -0,0 +1,10 @@
+console.log('mantul');
+
+burger.onclick = () => {
+  console.log('jos');
+  if (menu.style.display === 'block') {
+    menu.style.display = 'none';
+  } else {
+    menu.style.display = 'block';
+  }
+};
