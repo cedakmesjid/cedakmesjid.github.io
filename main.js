c7fc5f4e37be940c6b0c104de88e52a3d7505b68
yuulye <yuulye@gmail.com>
2025-03-15 02:19:14 +0700

    adding qr code menu; toggle qr code

--- main.js
+++ main.js
@@ -1,10 +1,25 @@
-console.log('mantul');
+console.log('loading...');
 
 burger.onclick = () => {
-  console.log('jos');
   if (menu.style.display === 'block') {
     menu.style.display = 'none';
   } else {
     menu.style.display = 'block';
+  }
+};
+
+
+qrToggle.onclick = () => {
+  console.log('toggle QR Code!');
+  console.log("display", qr.style.display);
+  if (
+    qr.style.display === ''
+    ||
+    qr.style.display === 'none'
+  ) {
+    qr.style.display = 'inline';
+  } else {
+    console.log('bbbbb');
+    qr.style.display = 'none';
   }
 };
